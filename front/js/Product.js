61352697f15579e2b649b3ff2b2b62cd348190e0
Aure31880 <barbotin48@gmail.com>
2021-10-21 22:29:09 +0200

    creation des class Product et ServiceProduct

---
+++ front/js/Product.js
@@ -0,0 +1,15 @@
+class Product {
+    constructor(productList) {
+        productList && Object.assign(this, productList)
+    }
+
+    getPriceFormat(product) {
+        let price = this.price;
+        price = new Intl.NumberFormat("fr-FR", {
+            style: "currency",
+            currency: "EUR",
+            minimumFractionDigits: 2,
+        }).format(price);
+        return price;
+    }
+}
---
+++ front/js/ServiceProduct.js
@@ -0,0 +1,14 @@
+class ServiceProduct {
+
+    async fetchAll() {
+        return await fetch('http://localhost:3000/api/products')
+            .then(data => data.json())
+            .then(products => products.map(product => new Product(product)));
+    }
+
+    async fetchOne(productId) {
+        return await fetch(`http://localhost:3000/api/products/${productId}`)
+            .then(data => data.json())
+            .then(product => new Product(product));
+    }
+}
