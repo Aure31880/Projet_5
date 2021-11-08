0987f8accc1253e78881c5ff27d77c520ba7d2b2
Aure31880 <barbotin48@gmail.com>
2021-11-08 13:45:16 +0100

    page cart affichage total produits

--- front/components/cart.js
+++ front/components/cart.js
@@ -4,10 +4,10 @@ async function shoppingList() {
   const serviceProduct = new ServiceProduct();
 
   const cart = await Promise.all(serviceProduct.getShoppingList());
-  console.log(cart);
+  // console.log(cart);
 
   for (let prodCart of cart) {
-    console.log(prodCart);
+    // console.log(prodCart);
     document.getElementById("cart__items").innerHTML +=
       `
                 <article class="cart__item" ">
@@ -34,16 +34,60 @@ async function shoppingList() {
 
             `
 
-    const btnDel = document.getElementsByClassName('deleteItem');
-    for (let i = 0; i < btnDel.length; i++) {
-      btnDel[i].addEventListener('click', function (e) {
-        e.preventDefault();
-        let id = cart[i].id;
-        console.log(id);
-        serviceProduct.removeProduct(id)
-        console.log(cart);
-      })
+    function getAllProductQuantity() {
+      const getTotalProducts = document.querySelector(".itemQuantity");
+      const totalProducts = getTotalProducts.value;
+      // console.log(totalProducts);
+
+      let tab = [];
+
+      for (let prod of cart) {
+        const total = prod.quantity;
+        // console.log(total);
+
+        tab.push(total)
+      }
+
+      let tabParse = [];
+
+      const formatResult = Object.values(tab);
+      // console.log(formatResult);
+      formatResult.forEach(elements => {
+        // console.log(elements);
+        const resultParse = parseInt(elements);
+        tabParse.push(resultParse);
+        console.log(tabParse);
+
+        const reducer = (accumulator, curr) => accumulator + curr;
+        const showAllQuantity = tabParse.reduce(reducer);
+        document.getElementById('totalQuantity').innerHTML = showAllQuantity;
+
+      });
     }
+    getAllProductQuantity();
+
+    // Remove one Product from localStorage
+    function removeItem() {
+      const btnDel = document.getElementsByClassName('deleteItem');
+    }
+
+
+    function getTotalAmount() {
+
+    }
+
+    // const btnDel = document.getElementsByClassName('deleteItem');
+    // for (let i = 0; i < btnDel.length; i++) {
+    //   btnDel[i].addEventListener('click', function (e) {
+    //     e.preventDefault();
+    //     let id = cart[i].id;
+    //     console.log(id);
+    //     serviceProduct.removeProduct(id)
+    //     console.log(cart);
+    //   })
+    // }
+
+
 
   }
 
